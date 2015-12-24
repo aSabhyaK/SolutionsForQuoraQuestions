d28d2f5a06f47f0e434e217102a4d738382071ef
Sabhya Kaushal <sabhya001kaushal@gmail.com>
2015-12-24 12:50:23 +0530

    Create EscapeIt.java
    Posted as an answer to the following Quora question:
    
    https://www.quora.com/What-is-the-meaning-of-in-regular-expression

---
+++ EscapeIt.java
@@ -0,0 +1,8 @@
+class EscapeIt
+{
+    public static void main(String[] args) {
+        System.out.println("\nabc");
+        System.out.println("\\nabc");
+        System.out.println("\\\nabc");
+    }
+}
