8aa7ff88dd9af4d47f54db215979234dc9f55fab
Sabhya Kaushal <sabhya001kaushal@gmail.com>
2015-12-24 15:18:23 +0530

    Create StringFormatting.py

---
+++ Python/StringFormatting.py
@@ -0,0 +1,3 @@
+    print("I am %s and I have %d galleons in my Gringotts bank account!" % ('Harry Potter', 24));
+    print("Harry caught %s snitches, and Ron saved %d goals in Quidditch. Amazing!" % (4, 3));
+    print("My %d animagi: \tMickey mouse and \tBlue whales!" % (2));
