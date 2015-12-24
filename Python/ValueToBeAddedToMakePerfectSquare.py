e80d01475877fda531cf6ab6c4912da7395d2139
Sabhya Kaushal <sabhya001kaushal@gmail.com>
2015-12-24 15:24:56 +0530

    Create ValueToBeAddedToMakePerfectSquare.py
    Posted as an answer to the following Quora question:
    
    https://www.quora.com/Which-smallest-number-should-be-added-to-74684160-to-make-it-a-perfect-square

---
+++ Python/ValueToBeAddedToMakePerfectSquare.py
@@ -0,0 +1,6 @@
+    from math import sqrt, ceil;
+     
+    number_under_consideration = 74684160;
+    square_root = int(ceil(sqrt(number_under_consideration)));
+     
+    print(square_root * square_root - number_under_consideration);
