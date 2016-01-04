b3fd22b6c627d8441e75c4a0cd458b35cd32d39e
Sabhya Kaushal <sabhya001kaushal@gmail.com>
2016-01-04 15:14:22 +0530

    Update and rename AnagramCheckUsingHashMap.java to AnagramCheck.java

--- Java/AnagramCheck.java
+++ Java/AnagramCheck.java
@@ -1,20 +1,39 @@
     import java.util.HashMap;
     import java.util.Map;
      
-    public class AnagramCheckUsingHashMap {
+    public class AnagramCheck {
     	public static void main(String[] args) {
     		String three = "dorm";
     		String four = "mordor";
     		//LOTR refernce
-    		System.out.println(areAnagrams(three, four));
+    		System.out.println(areAnagramsHashMap(three, four));
+    		System.out.println(areAnagramsArray(three, four));
     		
     		String five = "tom marvolo riddle";
     		String six = "iam lord voldemort";
     		//the famous Harry Potter reference
-    		System.out.println(areAnagrams(five, six));
+    		System.out.println(areAnagramsHashMap(five, six));
+    		System.out.println(areAnagramsArray(five, six));
     	}
     	
-    	private static boolean areAnagrams(String one, String two)
+    	private static boolean areAnagramsArray(String one, String two)
+        {
+    	    int[] array = new int[256];
+    		
+    	    for(char c: one.toCharArray())
+    	    	array[c] ++;
+    		
+        	for(char c: two.toCharArray())
+        		array[c] --;
+    		
+        	for(int i = 0; i < 256; ++ i)
+        		if(array[i] != 0)
+        			return false;
+    		
+    	    return true;
+        }
+    	
+    	private static boolean areAnagramsHashMap(String one, String two)
     	{
     		Map<Character, Integer> map = new HashMap<>();
     		
